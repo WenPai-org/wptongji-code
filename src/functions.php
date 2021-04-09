c120921f444f24a3be25b0c76c30365db6bfbf39
sunxiyuan <sxy@ibadboy.net>
2021-04-09 12:45:03 +0800

    特性（函数库）：全局函数增加重复声明检测

--- src/functions.php
+++ src/functions.php
@@ -5,12 +5,14 @@
  * @package WenPai\TongJi
  */
 
-function wptj_get_option( string $option, string $section, $default = '' ) {
-    $options = is_multisite() ? get_site_option( WPTJ_PREFIX . "_{$section}") : get_option( WPTJ_PREFIX . "_{$section}" );
+if ( ! function_exists( 'wptj_get_option' ) ) {
+    function wptj_get_option( string $option, string $section, $default = '' ) {
+        $options = is_multisite() ? get_site_option( WPTJ_PREFIX . "_{$section}" ) : get_option( WPTJ_PREFIX . "_{$section}" );
 
-    if ( isset( $options[ $option ] ) ) {
-        return $options[ $option ];
+        if ( isset( $options[ $option ] ) ) {
+            return $options[ $option ];
+        }
+
+        return $default;
     }
-
-    return $default;
 }
